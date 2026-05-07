eaadfa19b77973994df2efd68cc9e6ceabeced42
Danil Burov <117172242+Danilburov@users.noreply.github.com>
2026-05-07 14:01:32 +0200

    Fix: Typos

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -9,11 +9,11 @@ terraform {
   required_providers {
     aws={
         source = "hashicorp/aws"
-        version = "-> 5.0"
+        version = "~> 5.0"
     }
     tls = {
       source = "hashicorp/tls"
-      version = "-> 4.0"
+      version = "~> 4.0"
     }
   }
 }
